3b9dd8ad69003c5a4135875ddb61abd3e9569338
sajanbista1030@gmail.com <sajanbista1030@gmail.com>
2025-05-16 17:26:10 +0545

    Initial Django project setup

--- store/models.py
+++ store/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    age_group = models.CharField(max_length=20)
+    category = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
--- store/views.py
+++ store/views.py
@@ -1,3 +1,6 @@
+# store/views.py
+
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
